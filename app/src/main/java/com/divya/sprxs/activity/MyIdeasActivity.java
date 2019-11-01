83514e65c2234057ab080489a245582de4356c24
divyaf9 <divya.prabakaran@fifth-9.com>
2019-11-01 15:06:27 +0000

    uptodate

---
+++ app/src/main/java/com/divya/sprxs/activity/MyIdeasActivity.java
@@ -0,0 +1,14 @@
+package com.divya.sprxs.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyIdeasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_ideas);
+    }
+}
---
+++ app/src/main/java/com/divya/sprxs/activity/NoIdeasActivity.java
@@ -0,0 +1,14 @@
+package com.divya.sprxs.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NoIdeasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_no_ideas);
+    }
+}
